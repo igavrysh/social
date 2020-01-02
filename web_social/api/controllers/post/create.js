ceea3de3aae6d040449185ceb8f541ff51f6a1db
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2020-01-01 21:10:22 -0800

    Add posts and post formatting to web app

--- web_social/api/controllers/post/create.js
+++ web_social/api/controllers/post/create.js
@@ -3,7 +3,8 @@ module.exports = async function(req, res) {
   console.log("Create post object with text: " + postBody);
 
   // Waterline creation syntax
-  const record = await Post.create({text: postBody}).fetch();
+  const userId = req.session.userId;
+  const record = await Post.create({text: postBody, user: userId}).fetch();
 
-  res.send(record);
+  res.redirect('/post');
 }
