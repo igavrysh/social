c6725e74833ec357ba0687e7dc013d88f7121309
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2020-01-03 08:46:38 -0800

    Initial ios project version

---
+++ ios_social/ios_socialTests/ios_socialTests.swift
@@ -0,0 +1,34 @@
+//
+//  ios_socialTests.swift
+//  ios_socialTests
+//
+//  Created by new on 1/3/20.
+//  Copyright © 2020 Ievgen Gavrysh. All rights reserved.
+//
+
+import XCTest
+@testable import ios_social
+
+class ios_socialTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
