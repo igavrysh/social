f75798dd64ec5ed02aa4d8fb432d1155df83b765
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2020-01-03 08:59:47 -0800

    Remove storyboard file, add HomeController

---
+++ ios_social/ios_social/Controllers/HomeController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeController.swift
+//  ios_social
+//
+//  Created by new on 1/3/20.
+//  Copyright © 2020 Ievgen Gavrysh. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .red
+    }
+}
