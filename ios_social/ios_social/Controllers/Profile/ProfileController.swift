503dede93d1f074ea729fb35b6aab92bacc22adb
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2020-01-12 21:25:30 -0800

    Profile header in public page for ios client

--- ios_social/ios_social/Controllers/Profile/ProfileController.swift
+++ ios_social/ios_social/Controllers/Profile/ProfileController.swift
@@ -19,11 +19,23 @@ extension ProfileController: UICollectionViewDelegateFlowLayout {
         return .init(width: view.frame.width, height: height)
     }
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        minimumLineSpacingForSectionAt section: Int
+    ) -> CGFloat {
         return 0
     }
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        referenceSizeForHeaderInSection section: Int
+    ) -> CGSize {
+        if user == nil {
+            return .zero
+        }
+        
         return .init(width: 0, height: 300)
     }
 }
@@ -33,6 +45,13 @@ class ProfileController: LBTAListHeaderController<UserPostCell, Post, ProfileHea
     var user: User?
     
     let userId: String
+    
+    fileprivate let activityIndicatorView: UIActivityIndicatorView = {
+        let aiv = UIActivityIndicatorView(style: .large)
+        aiv.startAnimating()
+        aiv.color = .darkGray
+        return aiv
+    }()
     
     init(userId: String) {
         self.userId = userId
@@ -47,12 +66,19 @@ class ProfileController: LBTAListHeaderController<UserPostCell, Post, ProfileHea
         super.viewDidLoad()
         
         fetchUserProfile()
+        
+        setupActivityIndicatorView()
     }
     
     override func setupHeader(_ header: ProfileHeader) {
         super.setupHeader(header)
-        header.fullNameLabel.text = user?.fullName
         
+        if user == nil {
+            return
+        }
+        
+        header.user = self.user
+        header.profileController = self
     }
     
     fileprivate func fetchUserProfile() {
@@ -61,25 +87,52 @@ class ProfileController: LBTAListHeaderController<UserPostCell, Post, ProfileHea
         Alamofire.request(url)
             .validate(statusCode: 200..<300)
             .responseData { (dataResp) in
-            //self.activityIndicatorView.stopAnimating
-            
-            if let err = dataResp.error {
-                print("Failed to fetch user profile: ", err)
-                return
-            }
-            
-            let data = dataResp.data ?? Data()
-            
-            do {
-                let user = try JSONDecoder().decode(User.self, from: data)
-                self.user = user
-                self.items = user.posts ?? []
-                //self.user = user
-                //self.items = user.posts ?? []
+                self.activityIndicatorView.stopAnimating()
+
+                if let err = dataResp.error {
+                    print("Failed to fetch user profile: ", err)
+                    return
+                }
+
+                let data = dataResp.data ?? Data()
+
+                do {
+                    let user = try JSONDecoder().decode(User.self, from: data)
+                    self.user = user
+                    self.items = user.posts ?? []
+                    //self.user = user
+                    //self.items = user.posts ?? []
+                    self.collectionView.reloadData()
+                } catch {
+                    print("Failed to decode user: ", error)
+                }
+        }
+    }
+    
+    fileprivate func setupActivityIndicatorView() {
+        collectionView.addSubview(activityIndicatorView)
+        activityIndicatorView.anchor(
+            top: collectionView.topAnchor,
+            leading: view.leadingAnchor,
+            bottom: nil,
+            trailing: view.trailingAnchor,
+            padding: .init(top: 100, left: 0, bottom: 0, right: 0))
+    }
+    
+    func handleFollowUnfollow() {
+        guard let user = user else { return }
+        let isFollowing = user.isFollowing == true
+        let url = "\(Service.shared.baseUrl)/\(isFollowing ? "unfollow" : "follow")/\(user.id)"
+        
+        Alamofire.request(url, method: .post)
+            .validate(statusCode: 200..<300).responseData { (dataRes) in
+                if let err = dataRes.error {
+                    print("Failed to unfollow", err)
+                    return
+                }
+                
+                self.user?.isFollowing = !isFollowing
                 self.collectionView.reloadData()
-            } catch {
-                print("Failed to decode user: ", error)
-            }
         }
     }
 }
--- ios_social/ios_social/Views/ProfileHeader.swift
+++ ios_social/ios_social/Views/ProfileHeader.swift
@@ -6,17 +6,113 @@
 //  Copyright © 2020 Ievgen Gavrysh. All rights reserved.
 //
 
-import UIKit
+import LBTATools
 
 class ProfileHeader: UICollectionReusableView {
-        
-    let fullNameLabel = UILabel(text: "Full name", font: .boldSystemFont(ofSize: 14), textColor: .black, textAlignment: .center)
+    
+    let profileImageView = CircularImageView(width: 80)
+    
+    let followButton = UIButton(
+        title: "Follow",
+        titleColor: .black,
+        font: .boldSystemFont(ofSize: 13),
+        target: self,
+        action: #selector(handleFollow))
+    
+    let postsCountLabel = UILabel(
+        text: "12",
+        font: .boldSystemFont(ofSize: 14),
+        textAlignment: .center)
+    let postsLabel = UILabel(
+        text: "posts",
+        font: .systemFont(ofSize: 13),
+        textColor: .lightGray,
+        textAlignment: .center)
+    
+    let followersCountLabel = UILabel(text: "500", font: .boldSystemFont(ofSize: 14), textAlignment: .center)
+    let followersLabel = UILabel(
+        text: "followers",
+        font: .systemFont(ofSize: 13),
+        textColor: .lightGray,
+        textAlignment: .center)
+    
+    let followingCountLabel = UILabel(text: "500", font: .boldSystemFont(ofSize: 14), textAlignment: .center)
+    let followingLabel = UILabel(
+        text: "following",
+        font: .systemFont(ofSize: 13),
+        textColor: .lightGray,
+        textAlignment: .center)
+    
+    let fullnameLabel = UILabel(text: "Username", font: .boldSystemFont(ofSize: 14))
+    let bioLabel = UILabel(
+        text: "Here's an interesting piece of  bio that will definitely capute your attention and all the fans around the world.",
+        font: .systemFont(ofSize: 13),
+        textColor: .darkGray,
+        numberOfLines: 0)
+    
+    
+    let fullNameLabel = UILabel(
+        text: "Full name",
+        font: .boldSystemFont(ofSize: 14),
+        textColor: .black,
+        textAlignment: .center)
+    
+    var user: User! {
+        didSet {
+            profileImageView.image = UIImage(named: "user")
+            fullnameLabel.text = user.fullName
+            
+            followButton.setTitle(user.isFollowing == true ? "Unfollow" : "Follow", for: .normal)
+            followButton.backgroundColor = user.isFollowing == true ? .black : .white
+            followButton.setTitleColor(user.isFollowing == true ? .white : .black, for: .normal)
+            
+            postsCountLabel.text = "\(user.posts?.count ?? 0)"
+            followersCountLabel.text = "\(user.followers?.count ?? 0)"
+            followingCountLabel.text = "\(user.following?.count ?? 0)"
+        }
+    }
+    
+    weak var profileController: ProfileController?
+    
+    let separatorView = UIView(backgroundColor: .init(white: 0.4, alpha: 0.3))
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .green
         
-        stack(fullNameLabel)
+        profileImageView.isUserInteractionEnabled = true
+        
+        followButton.layer.cornerRadius = 15
+        followButton.layer.borderWidth = 1
+        
+        profileImageView.layer.cornerRadius = 40
+        profileImageView.layer.borderWidth = 1
+                
+        stack(
+            profileImageView,
+            followButton.withSize(.init(width: 100, height: 28)),
+            hstack(
+                stack(postsCountLabel, postsLabel),
+                stack(followersCountLabel, followersLabel),
+                stack(followingCountLabel, followingLabel),
+                spacing: 16,
+                alignment: .center
+            ),
+            fullnameLabel,
+            bioLabel,
+            spacing: 12,
+            alignment: .center).withMargins(.allSides(14))
+        
+        addSubview(separatorView)
+        separatorView.anchor(
+            top: nil,
+            leading: leadingAnchor,
+            bottom: bottomAnchor,
+            trailing: trailingAnchor,
+            size: .init(width: 0, height: 0.5))
+    }
+    
+    @objc fileprivate func handleFollow() {
+        profileController?.handleFollowUnfollow()
     }
     
     required init?(coder: NSCoder) {
